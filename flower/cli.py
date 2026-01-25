ff01a46a1b124425f534cf431520699651bffe53
watari <watari@techwebunraveled.xyz>
2026-01-25 17:17:25 +0300

    development - argparse

--- flower/cli.py
+++ flower/cli.py
@@ -3,43 +3,62 @@
 import pathlib
 import argparse
 
-parser = argparse.ArgumentParser(description="flower — a simple, declarative video editor")
+class CustomFormatter(
+    argparse.ArgumentDefaultsHelpFormatter,
+    argparse.RawTextHelpFormatter
+):
+    pass
 
 def parse_args():
+
+    parser = argparse.ArgumentParser(
+        description="🌷 flower — a simple, declarative video editor 🌷",
+        formatter_class=CustomFormatter,
+        epilog = "Example:\n  flower -i my_project -o order.yaml -w final.mp4"
+    )
+
     parser.add_argument(
         "-i",
         "--init",
         metavar="PROJECT",
         help="Initialize a new project directory",
-        nargs=1,
-        type=str
+        type=str,
     )
 
     parser.add_argument(
         "-o",
         "--order",
         metavar="ORDERFILE",
-        help="""Order file to use (Default: order.yaml)""",
+        help="Order file to use",
         default="order.yaml",
-        nargs=1,
-        type=str
+        type=str,
     )
 
     parser.add_argument(
         "-w",
         "--write",
         metavar="OUTPUTFILE",
-        help="""Output video filename (default: output.mp4)""",
+        help="""Output video filename""",
         default="output.mp4",
-        nargs=1,
-        type=str
+        type=str,
+    )
+
+    parser.add_argument(
+        "-p",
+        "--project",
+        metavar="PROJECTDIR",
+        help="""Project directory to render""",
+        default="media",
+        type=str,
     )
 
     return parser.parse_args()
 
+
 def main():
     args = parse_args()
     print(args)
+
 
 if __name__ == "__main__":
     main()
